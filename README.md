f2efe4c6f03fd78460b6d37de872f3efa5d03903
Matt Gates <mgates@mail.bradley.edu>
2020-01-17 17:51:30 -0500

    readme changes and some other minor stuff

--- README.md
+++ README.md
@@ -1,72 +1,13 @@
-# jterm-dis
+# 9toes.club
 
-This is my January term Directed Independent Study project. The end product is a React CRUD app that allows users to submit, edit, delete, and view recipes. There is also register and login functionality.
+This is my independent study project from January 2020. It's my first attempt at a CRUD app and also my first time using JavaScript, React, and making an API.
 
 ## About the project
 
-All of the front-end and back-end has been done using JavaScript and mainly using the React library. I've also created an API with Express that manages all of the communication with MongoDB Atlas.
+All of the front-end has been done using JavaScript and mainly using the React library. I've also created an API with Express that manages all of the communication with MongoDB Atlas where all account and recipe data is stored.
 
 The project is hosted on a Raspberry Pi that is configured to serve the React app to the domain name [9toes.club](http://9toes.club).
 
 ## Setting Up the Raspberry Pi
 
-For this project I'm using a Raspberry Pi 4 Model B 2019 Quad Core 64 Bit WiFi Bluetooth (4GB). The operating system is Raspbian GNU/Linux 10 (buster) which can be downloaded for free on the [Raspberry Pi](https://www.raspberrypi.org/downloads/) website.
-
-A program called Nginx is being used as a reverse proxy in order to serve the website on the Pi. On the router in my apartment I've setup the firewall to only accept requests to port 80 and they are forwarded to the internal IP address of the Pi. From there, Nginx takes those requests and directs them to the port where the React app is running. The Express API is running on a separate port and any request that go through 9toes.club/api are then proxied to the Express API port.
-
-In order to configure Nginx, the file located at /etc/nginx/sites-available/default must be configured properly. This directs all traffic from port 80 to port 5000 where the React app lives. Then it takes all /api traffic and proxies that to port 3000 for the Express API.
-
-```
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-
-	root /usr/share/nginx/html;
-
-	server_name 9toes.club;
-
-	location / {
-            proxy_pass http://localhost:5000;
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection 'upgrade';
-            proxy_set_header Host $host;
-            proxy_cache_bypass $http_upgrade;
-	}
-
-	location /api/ {
-	    proxy_pass http://localhost:3000;
-	    proxy_http_version 1.1;
-	    proxy_set_header Upgrade $http_upgrade;
-	    proxy_set_header Connection 'upgrade';
-	    proxy_set_header Host $host;
-	    proxy_cache_bypass $http_upgrade;
-	}
-}
-```
-
-After Nginx has been configured, the following command is used in terminal to check if the configuration is valid.
-
-`sudo nginx -t`
-
-This last command restarts Nginx with the new settings.
-
-`sudo systemctl restart -nginx`
-
-## Replicating the Project
-
-In order to run the code from the github repository you'll need to make some changes and install a few libraries/frameworks.
-
-For the Express API:
-
-`npm install express mongoose dotenv bcryptjs @hapi/joi`
-
-For the React App:
-
-`npx create-react-app my-app`
-
-`npm install js-sha256 js-cookie react-bootstrap react-router-dom formik yup`
-
-There are a number of files that are included in the .gitignore for security reasons, but the main information that you'd have to replicate is the MongoDB communication string (DB_CONNECT). This are located in a .env file and you can reference the [dotenv documentation](https://www.npmjs.com/package/dotenv). Any other files included in the .gitignore that you may need should be created when running
-
-`npx create-react-app my-app`
+For this project I'm using a Raspberry Pi 4 Model B 2019 Quad Core 64 Bit WiFi Bluetooth (4GB). The operating system is Raspbian GNU/Linux 10 (buster). I'm using NGINX as reverse proxy to direct traffic to the React app and then for contact with the API.
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -12,18 +12,18 @@ class Home extends React.Component {
             paddingBottom: 1
           }}
         >
-          <h1>jterm-dis</h1>
+          <h1>9toes.club</h1>
           <p>
-            This is my January term Directed Independent Study project. The end
-            product is a React CRUD app that allows users to submit, edit,
-            delete, and view recipes. There is also register and login
-            functionality.
+            This is my independent study project from January 2020. It's my
+            first attempt at a CRUD app and also my first time using JavaScript,
+            React, and making an API.
           </p>
           <h2>About the Project</h2>
           <p>
-            All of the front-end and back-end has been done using JavaScript and
-            mainly using the React library. I've also created an API with
-            Express that manages all of the communication with MongoDB Atlas.
+            All of the front-end has been done using JavaScript and mainly using
+            the React library. I've also created an API with Express that
+            manages all of the communication with MongoDB Atlas where all
+            account and recipe data is stored.
             <br />
             <br />
             The project is hosted on a Raspberry Pi that is configured to serve
@@ -34,113 +34,8 @@ class Home extends React.Component {
           <p>
             For this project I'm using a Raspberry Pi 4 Model B 2019 Quad Core
             64 Bit WiFi Bluetooth (4GB). The operating system is Raspbian
-            GNU/Linux 10 (buster) which can be downloaded for free on the{" "}
-            <a href="https://www.raspberrypi.org/downloads/">Raspberry Pi</a>{" "}
-            website.
-            <br />
-            <br />
-            A program called Nginx is being used as a reverse proxy in order to
-            serve the website on the Pi. On the router in my apartment I've
-            setup the firewall to only accept requests to port 80 and they are
-            forwarded to the internal IP address of the Pi. From there, Nginx
-            takes those requests and directs them to the port where the React
-            app is running. The Express API is running on a separate port and
-            any request that go through 9toes.club/api are then proxied to the
-            Express API port.
-            <br />
-            <br />
-            In order to configure Nginx, the file located at
-            /etc/nginx/sites-available/default must be configured properly. This
-            directs all traffic from port 80 to port 5000 where the React app
-            lives. Then it takes all /api traffic and proxies that to port 3000
-            for the Express API.
-            <br />
-            <br />
-            server {"{"} <br />
-            listen 80 default_server;
-            <br />
-            listen [::]:80 default_server;
-            <br />
-            root /usr/share/nginx/html;
-            <br />
-            server_name 9toes.club;
-            <br />
-            <br />
-            location / {"{"}
-            <br />
-            proxy_pass http://localhost:5000;
-            <br />
-            proxy_http_version 1.1;
-            <br />
-            proxy_set_header Upgrade $http_upgrade;
-            <br />
-            proxy_set_header Connection 'upgrade';
-            <br />
-            proxy_set_header Host $host;
-            <br />
-            proxy_cache_bypass $http_upgrade;
-            <br />
-            }<br />
-            <br />
-            location /api/ {"{"}
-            <br />
-            proxy_pass http://localhost:3000;
-            <br />
-            proxy_http_version 1.1;
-            <br />
-            proxy_set_header Upgrade $http_upgrade;
-            <br />
-            proxy_set_header Connection 'upgrade';
-            <br />
-            proxy_set_header Host $host;
-            <br />
-            proxy_cache_bypass $http_upgrade;
-            <br />
-            }<br />
-            }
-            <br />
-            <br />
-            After Nginx has been configured, the following command is used in
-            terminal to check if the configuration is valid.
-            <br />
-            `sudo nginx -t`
-            <br />
-            <br />
-            This last command restarts Nginx with the new settings.
-            <br />
-            `sudo systemctl restart -nginx`
-          </p>
-          <h2>Replicating the Project</h2>
-          <p>
-            In order to run the code from the github repository you'll need to
-            make some changes and install a few libraries/frameworks.
-            <br />
-            <br />
-            For the Express API:
-            <br />
-            `npm install express mongoose jsonwebtoken dotenv bcryptjs
-            @hapi/joi`
-            <br />
-            <br />
-            For the React App:
-            <br />
-            `npx create-react-app my-app`
-            <br />
-            `npm install js-sha256 js-cookie react-bootstrap react-router-dom
-            formik yup`
-            <br />
-            <br />
-            There are a number of files that are included in the .gitignore for
-            security reasons, but the main information that you'd have to
-            replicate is the MongoDB communication string (DB_CONNECT). This are
-            located in a .env file and you can reference the{" "}
-            <a href="https://www.npmjs.com/package/dotenv">
-              dotenv documentation
-            </a>
-            . Any other files included in the .gitignore that you may need
-            should be created when running
-            <br />
-            `npx create-react-app my-app`
+            GNU/Linux 10 (buster). I'm using NGINX as reverse proxy to direct
+            traffic to the React app and then for contact with the API.
           </p>
         </Jumbotron>
       </div>
